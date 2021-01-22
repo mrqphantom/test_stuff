4b39d88dfd3b278630f83629c47bc65140245807
DESKTOP-J6I4MVD\mrq <hongquang.mh@gmail.com>
2021-01-22 07:05:08 +0700

    update anim chem

---
+++ Assets/script/playerAttack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAttack : MonoBehaviour
+{
+    public bool Attacking = false;
+    public float timedelay = 0.3f;
+    public Animator anim;
+    public Collider2D trigger;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        trigger.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && !Attacking)
+        {
+            Attacking = true;
+            trigger.enabled = true;
+            timedelay = 0.3f;
+        }
+        if (Attacking)
+        {
+            {
+                if (timedelay > 0)
+                {
+                    timedelay -= Time.deltaTime;
+                }
+
+                else
+                {
+                    Attacking = false;
+                    trigger.enabled = false;
+                }
+            }
+            anim.SetBool("Attacking", Attacking);
+
+        }
+    }
+}
