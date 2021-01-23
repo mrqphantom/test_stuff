c5c02f1b819883b4fc36152b92f90aaadeb44153
DESKTOP-J6I4MVD\mrq <hongquang.mh@gmail.com>
2021-01-23 12:23:46 +0700

    hinh anh attack

---
+++ Assets/script/Box.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public int health = 100;
+    // Start is called before the first frame update
+   
+   
+
+    // Update is called once per frame
+    void Update()
+    { 
+         if (health<=0)
+        {
+            Destroy(gameObject);
+        }
+    
+        void takedamage(int damage)
+        {
+            health -= damage;
+        }
+    }
+}
---
+++ Assets/script/attackTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attackTrigger : MonoBehaviour
+{
+    public int damage = 20;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.isTrigger != false && col.CompareTag("Enemy"))
+        {
+            Debug.Log("Cham roi");
+            col.SendMessageUpwards("damage", damage);
+        }
+    }
+}
